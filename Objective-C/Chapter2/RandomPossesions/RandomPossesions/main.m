cfcf4abf90b66395aee2552f2721901f2c013ac7
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-16 16:40:33 -0400

    Setup chapter 2 project

---
+++ Objective-C/Chapter2/RandomPossesions/RandomPossesions/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  RandomPossesions
+//
+//  Created by Arie Myrmo on 8/16/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+
+        // Destroy the array pointed to by items
+        items = nil;
+    }
+    return 0;
+}
