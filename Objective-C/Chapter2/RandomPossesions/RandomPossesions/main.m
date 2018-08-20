bebe3fe5c9630fbbd2bb02f29ac27b4523fdbaba
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-20 16:01:13 -0400

    clean up files and update init in main

--- Objective-C/Chapter2/RandomPossesions/RandomPossesions/main.m
+++ Objective-C/Chapter2/RandomPossesions/RandomPossesions/main.m
@@ -15,38 +15,13 @@ int main(int argc, const char * argv[]) {
         // Create a mutable array object, store its address in items variable
         NSMutableArray *items = [[NSMutableArray alloc] init];
 
-        // Send message addObject: to the NSMutableArray pointed to
-        //  by the variable items, passing a string each time.
-        [items addObject:@"One"];
-        [items addObject:@"Two"];
-        [items addObject:@"Three"];
 
-        // Send another message, insertObject:atIndex, to that same array object
-        [items insertObject:@"Zero"
-                    atIndex:0];
+        BNRItem *p = [[BNRItem alloc] initWithItemName:@"Red Sofa"
+                                        valueInDollars:100
+                                          serialNumber:@"A1B2C"];
 
-        // For every item in the array as determined by sending count to items
-        for (int i = 0;  i < [items count]; i++)
-        {
-            // We get the ith object from the array and pass it as an argument to
-            //  NSLog, which implicitly sends the description message to that object
-            NSLog(@"%@", [items objectAtIndex:i]);
-
-        }
-
-        BNRItem *p = [[BNRItem alloc] init];
-
-        // This creates a new NSString, "Red Sofa" and gives it to the BNRItem
-        [p setItemName:@"Red Sofa"];
-
-        [p setSerialNumber:@"A1B2C"];
-        [p setValueInDollars:100];
-
-        // Remember an NSLog with %@ as the token will print the
-        //  description of the corresponding argument
         NSLog(@"%@", p);
 
-        // Destroy the array pointed to by items
         items = nil;
     }
     return 0;
