b88a10e0accc054781dfe72c8b31139b5ec98244
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-20 14:12:48 -0400

    Setup BNRItem header

---
+++ Objective-C/Chapter2/RandomPossesions/RandomPossesions/BNRItem.h
@@ -0,0 +1,28 @@
+//
+//  BNRItem.h
+//  RandomPossesions
+//
+//  Created by Arie Myrmo on 8/20/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *itemName;
+    NSString *serialNumber;
+    int valueInDollars;
+    NSDate *dateCreated;
+}
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)i;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+@end
---
+++ Objective-C/Chapter2/RandomPossesions/RandomPossesions/BNRItem.m
@@ -0,0 +1,9 @@
+//
+//  BNRItem.m
+//  RandomPossesions
+//
+//  Created by Arie Myrmo on 8/20/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
