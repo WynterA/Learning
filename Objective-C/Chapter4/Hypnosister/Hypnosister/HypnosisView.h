9b5c8374866df4b7c5255297bdbe58280c0d2da9
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-28 15:36:27 -0400

    Add view

---
+++ Objective-C/Chapter4/Hypnosister/Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Arie Myrmo on 8/28/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ Objective-C/Chapter4/Hypnosister/Hypnosister/HypnosisView.m
@@ -0,0 +1,14 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Arie Myrmo on 8/28/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+
+@end
