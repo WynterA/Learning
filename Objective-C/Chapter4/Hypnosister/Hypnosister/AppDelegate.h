474d9c7abd12cb51572e41d4681225bdf1ec5edf
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-28 13:47:11 -0400

    Add Hypnosister project

---
+++ Objective-C/Chapter4/Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Arie Myrmo on 8/28/18.
+//  Copyright © 2018 Myrmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
  - Chapter 1: Quiz
  - Chapter 2: RandomPossesions
  - Chapter 3: RandomPossesions
- - Chapter 4: Whereami
+ - Chapter 4: Hypnosister
  
  
 
