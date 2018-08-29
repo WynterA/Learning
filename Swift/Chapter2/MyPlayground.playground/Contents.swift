e75280ca6ae5e882bd315c3b9a712a02db6022de
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-29 16:29:23 -0400

    Finish up

--- Swift/Chapter2/MyPlayground.playground/Contents.swift
+++ Swift/Chapter2/MyPlayground.playground/Contents.swift
@@ -21,7 +21,7 @@ var winningLotteryNumbers: Set<Int>
 // Literals and subscripting
 let number = 42
 let fmStation = 91.1
-let countingUp = ["one", "two"]
+var countingUp = ["one", "two"]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 let secondElement = countingUp[1]
 
@@ -36,6 +36,69 @@ let meaningOfLife = String(number)
 let availableRooms = Set([205, 411, 412])
 let defaultFloat = Float()
 let floatFromLiteral = Float(3.14)
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
+
+// Properties
+countingUp.count
+emptyString.isEmpty
+
+// Instance methods
+countingUp.append("three")
+
+// Optionals
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+reading1 = 9.8
+reading2 = 9.2
+reading3 = 9.7
+if let r1 = reading1,
+    let r2 = reading2,
+    let r3 = reading3 {
+        let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+}
+
+// Subscripting dictionaries
+if let space13Assignee = nameByParkingSpace[13] {
+    print("Key 13 is assigned in the dictionary!")
+}
+
+// Loops and String Interpolation
+for (space, name) in nameByParkingSpace {
+    let permit = "Space \(space): \(name)"
+}
+
+// Enums and Switch Statement
+enum PieType: Int {
+    case apple = 0
+    case cherry
+    case pecan
+}
+
+let favoritePie = PieType.apple
+
+let name: String
+switch favoritePie {
+case .apple:
+    name = "Apple"
+case .cherry:
+    name = "Cherry"
+case .pecan:
+    name = "Pecan"
+}
+
+let pieRawValue = PieType.pecan.rawValue
+// pieRawValue is an Int with a value of 2
+
+if let pieType = PieType(rawValue: pieRawValue) {
+    // Got a valid 'pieType'!
+}
+
+
 
 
 
