5c1818abd5b17d214c72e54ae0808743414e979c
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-29 16:06:14 -0400

    Add playground for chapter 2

---
+++ Swift/Chapter2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+// Learning - Swift Chapter 2
+
+import UIKit
+import PlaygroundSupport
+
+var str = "Hello, playground"
+str = "Hello, Swift"
+let constStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+
+// Collection Types
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNumbers: Set<Int>
+
+// Literals and subscripting
+let number = 42
+let fmStation = 91.1
+let countingUp = ["one", "two"]
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+let secondElement = countingUp[1]
+
+// Initializers
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+let defaultNumber = Int()
+let defaultBool = Bool()
+// multiple initializers
+let meaningOfLife = String(number)
+let availableRooms = Set([205, 411, 412])
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+
+
