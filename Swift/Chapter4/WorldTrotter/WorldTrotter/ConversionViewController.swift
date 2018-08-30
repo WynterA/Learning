e36e3bf065787441a6682b51c190db3083ec4801
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-30 15:53:00 -0400

    dismiss keyboard

--- Swift/Chapter4/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Swift/Chapter4/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, !text.isEmpty {
@@ -17,5 +19,9 @@ class ConversionViewController: UIViewController {
         } else {
             celsiusLabel.text = "???"
         }
+    }
+
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
 }
