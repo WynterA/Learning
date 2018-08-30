07e09efe8a08deb6d5b9f9a578763bc4afd73f11
Arie Myrmo <arie.myrmo@arthrex.com>
2018-08-30 15:49:16 -0400

    handle no text

--- Swift/Chapter4/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ Swift/Chapter4/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -12,6 +12,10 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 }
