a3e488261b7bbf21609a30c6e01a07e1482c5b44
Arie Myrmo <arie.myrmo@arthrex.com>
2018-10-04 10:55:16 -0400

    Homepwner project setup

---
+++ Swift/Chapter9/Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Arie Myrmo on 10/4/18.
+//  Copyright © 2018 WynterVate. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
---
+++ Swift/Chapter9/Homepwner/Homepwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homepwner
+//
+//  Created by Arie Myrmo on 10/4/18.
+//  Copyright © 2018 WynterVate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
